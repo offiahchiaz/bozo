9e126a80b45e54cb2abffde1c2ea9918228ec7c0
Offiahchiaz <offiahchiaz@gmail.com>
2019-02-05 23:22:31 +0100

    Add getBooksQuery

--- server/app.js
+++ server/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const graphqlHTTP = require('express-graphql');
+const cors = require('cors');
 
 const schema = require('./schema/schema');
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+// allow cros-origin requests
+app.use(cors());
 
 mongoose.connect('mongodb://localhost:27017/bozo', {useNewUrlParser: true})
     .then(() => console.log('Database Connected'))
